33d7561f55a5c649079c9a9654f074c046e3f545
mehul chopra <Mehulc@nextgen.com>
2020-01-16 08:05:30 +0530

    day 9

---
+++ dict_play.py
@@ -0,0 +1,25 @@
+from com.abc.college.student import Student
+
+sdict = {
+    10: Student(name='mehul', gender='m', roll=10, marks=90),
+    5: Student(name='muhammad', gender='m', roll=5, marks=56),
+    12: Student(name='jane', gender='f', roll=12, marks=88),
+    4: Student(name='jill', gender='f', roll=4, marks=99)
+}
+
+# get a new dict of items (rollno: name) from sdict
+''' result = {}
+items = sdict.items()
+for item in items:
+    result[item[0]] = item[1].name
+
+print(result) '''
+
+results = {roll: student.name for roll, student in sdict.items()}
+print(results)
+
+# get a new dict of items (rollno: (name, gender)) from sdict for only those students whose marks scored is > 88
+ans = {roll: (student.name, student.gender) for roll, student in sdict.items() if student.marks > 88}
+print(ans)
+
+# get a new dict of items (rollno: [name, marks]) from sdict for only those students who have the gender 'm'
--- search_student.py
+++ search_student.py
@@ -1,19 +1,33 @@
 from com.abc.college.student import Student
 
-slist = [
+# ordered data structure
+''' slist = [
     Student(name='mehul', gender='m', roll=10, marks=90),
     Student(name='muhammad', gender='m', roll=5, marks=56),
     Student(name='jane', gender='f', roll=12, marks=88),
     Student(name='jill', gender='f', roll=4, marks=99)
-]
+] '''
+
+sdict = {
+    10: Student(name='mehul', gender='m', roll=10, marks=90),
+    5: Student(name='muhammad', gender='m', roll=5, marks=56),
+    12: Student(name='jane', gender='f', roll=12, marks=88),
+    4: Student(name='jill', gender='f', roll=4, marks=99)
+}
 
 roll = int(input('Enter roll to search: '))
 
-for student in slist:
+''' for student in slist:
     if student.roll == roll:
         print(student.get_details())
         break
 else:
     # will execute if the corresponding for block is completely exhausted
     # no break has been encountered in the corresponding for block
+    print('Student not found') '''
+
+if roll in sdict:
+    student = sdict[roll]
+    print(student.get_details())
+else:
     print('Student not found')
